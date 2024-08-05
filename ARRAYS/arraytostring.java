017a2e9230c888c66b15817b550e697c97554852
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-08-05 10:55:55 +0530

    java

--- ARRAYS/arraytostring.java
+++ ARRAYS/arraytostring.java
@@ -1,27 +1,13 @@
+
+import java.util.Arrays;
+
 public class arraytostring {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        System.out.println(toString(arr));
+       int[] nums ={1,2,3,4,5};
+       System.out.println(Arrays.toString(nums)); 
+}
+    static void change(int[] arr){
+        arr[0]=99;
     }
 
-    
-    public static String toSTring(int[] a){
-        if(a==null){
-            return "null";
-        int iMax=a.length-1;
-        if(iMax==-1){
-            return "[]";
-
-    StringBuilder b = new StringBuilder();
-    b.append('[');
-    for(int i=0; ;i++){
-        b.append(a[i]);
-        if(i==iMax)
-        return b.append(']').toString();
-        b.append(",");
-    }
-        }
-        }
-    }
-    
 }
