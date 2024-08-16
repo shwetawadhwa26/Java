ebe547f89ca21bf717aa89daa034a038b77a023e
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-08-16 10:52:43 +0530

    java

---
+++ SEARCHINGSORTING/linearsearch.java
@@ -0,0 +1,13 @@
+//linear search - sequentially checks each element in the list until the target
+// value is found or the end of the list is reached. If the target
+// is found, the algorithm returns its index; otherwise, it returns
+// a value indicating the target is not present.
+
+//TIME COMPLXITY - 
+//BEST - O(1)   - only 1 comparison is needed    
+//WORST - O(N)  - iterate till last item and still cant find the element  
+
+//QUES - How many checks will the loop make in the best case ie element found at nth index
+//arr=[8,9,11,12,18,...200 elements]
+//target = 8
+
