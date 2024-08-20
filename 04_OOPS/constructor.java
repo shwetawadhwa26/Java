b316faa46ed4314df5ce907dc443634dd8ed6fa2
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-08-20 21:11:23 +0530

    java

--- 04_OOPS/constructor.java
+++ 04_OOPS/constructor.java
@@ -1,5 +1,31 @@
-import java.util.Scanner;
 
 public class constructor {
+    class Person {
+    // Instance variables
+    String name;
+    int age;
+
+    // Constructor
+    Person(String name, int age) {
+        this.name = name;  // 'this.name' refers to the instance variable, 'name' is the parameter
+        this.age = age;    // 'this.age' refers to the instance variable, 'age' is the parameter
+    }
+
+    // Method to display the person's details
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        // Creating an object of the Person class
+        Person person1 = new Person("Alice", 30);
+
+        // Displaying the person's details using the display method
+        person1.display();
+    }
+}
+
+
     
 }
