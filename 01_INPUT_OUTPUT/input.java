607b58dbcae034b692f229508317c3afd3d8c906
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-08-22 09:42:09 +0530

    java

--- 01_INPUT_OUTPUT/input.java
+++ 01_INPUT_OUTPUT/input.java
@@ -4,7 +4,7 @@ public class input {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int rollno = input.nextInt();
-        System.out.println("your rollno is = "+rollno);
+        System.out.println("your rollno is = "+ rollno);
 
     
         long a = 230_99_999_999L;
---
+++ ARRAYS/QUESTIONS/length.java
@@ -0,0 +1,12 @@
+public class length {
+    public static void main(String[] args) {
+        
+    int[] ages = new int[3];
+    float[] weights = new float[3];
+    String[] names = {"rahul","raj","shweta","vaishali"};
+    System.out.println(ages.length);
+    System.out.println(weights.length);
+    System.out.println(names.length);
+    }
+}
+
