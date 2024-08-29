4da4c323766b51402289e06748e401d931dc6ae0
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-08-29 09:47:43 +0530

    strings

---
+++ STRINGS/comparsion.java
@@ -0,0 +1,12 @@
+public class comparsion {
+    public static void main(String[] args) {
+        String a = "Shweta";
+        String b = "Shweta";
+
+        //==
+        System.out.println(a==b);
+
+        
+    }
+    
+}
