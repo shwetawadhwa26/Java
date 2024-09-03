2b98adb14b3c440b68fde21b4f407816860cd802
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-03 10:56:31 +0530

    strings

---
+++ ARRAYS/QUESTIONS/LowercaseToUppercase.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LowercaseToUppercase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine(); // Read the entire line of input
+        char[] ch = input.toCharArray();   // Convert the string to a character array
+
+        for (int index = 0; index < ch.length; index++) {
+            char currCharacter = ch[index];
+            if (currCharacter >= 'A' && currCharacter <= 'Z') {
+                ch[index] = (char)(currCharacter +32);
+            }
+        }
+
+        String result = new String(ch);    // Convert the character array back to a string
+        System.out.println("Uppercase string: " + result);
+    }
+}
--- STRINGS/stringbuilder.java
+++ STRINGS/stringbuilder.java
@@ -1,14 +1,24 @@
-public class stringbuilder {
-    public static void main(String[] args) {
-        StringBuilder builder = new StringBuilder();
-        for(int i=0;i<26;i++){
-            char ch = (char)('a'+i);
-            builder.append(ch);
-        }
-        System.out.println(builder.toString());
-        builder.reverse();
-        System.out.println(builder);
+// public class stringbuilder {
+//     public static void main(String[] args) {
+//         StringBuilder builder = new StringBuilder();
+//         for(int i=0;i<26;i++){
+//             char ch = (char)('a'+i);
+//             builder.append(ch);
+//         }
+//         System.out.println(builder.toString());
+//         builder.reverse();
+//         System.out.println(builder);
         
-    }
+//     }
     
+// }
+
+public class stringbuilder{
+    public static void main(String[] args) {
+
+        for(int i =0;i<10;i++){
+            char ch=(char)('a'+i);
+            System.out.println(i+" "+ch[i]);
+        }
+    }
 }
