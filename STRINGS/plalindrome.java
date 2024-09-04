7992b7dd72118c69def64bfda0e07cad26358c40
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-04 09:55:47 +0530

    palindrome

--- STRINGS/plalindrome.java
+++ STRINGS/plalindrome.java
@@ -1,21 +1,23 @@
+import java.util.Scanner;
 public class plalindrome {
     public static void main(String[] args) {
-        String str ="shweta";
-        System.out.println(isPalidrome(str));
-        
-
-    }
-    static boolean isPalidrome(String str){
-        str = str.toLowerCase();
-        for(int i =0; i <=str.length()/2;i++){
-            char start = str.charAt(i);
-            char end = str.charAt(str.length()-1-i);
-            if(start!=end){
-                return false;
-
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+      //length of string 
+        char ch[]=input.toCharArray();
+        int n = ch.length;
+        int i =0;
+        int j =n-1;
+        while(i>=j){
+            if(ch[i]==ch[j]){
+                i++;
+                j--;
             }
+        System.out.println("the string is a palindrome");
+
         }
-        return true;
+
+        
     }
     
 }
