c13ddd1a331ab223613bebef6c762e406f7ab9bd
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-09 20:39:36 +0530

    fwd

--- 02_LOOPs/PrintFibonacci.java
+++ 02_LOOPs/PrintFibonacci.java
@@ -1,31 +1,25 @@
 public class PrintFibonacci {
-
-    // Method to print Fibonacci sequence up to the given count
     public static void printFibonacciSequence(int count) {
-        int a = 0; // First Fibonacci number
-        int b = 1; // Second Fibonacci number
-        int c;     // Variable to hold the next Fibonacci number
+        int a = 0; 
+        int b = 1; 
+        int c;     
 
         // Loop to print Fibonacci sequence
         for (int i = 1; i <= count; i++) {
-            System.out.print(a); // Print the current Fibonacci number
-            
-            // If it's not the last number, print a comma and space
+            System.out.print(a); 
             if (i < count) {
                 System.out.print(", ");
             }
-
-            // Update the variables to get the next number in the sequence
-            c = a + b; // Next number is the sum of the previous two
-            a = b;     // Move the second number to the first
-            b = c;     // Move the next number to the second
+            c = a + b; 
+            a = b;     
+            b = c;     
         }
     }
 
     public static void main(String[] args) {
-        int count = 10; // Specify how many Fibonacci numbers to print
+        int count = 10; 
         System.out.println("Fibonacci sequence up to " + count + " terms:");
-        printFibonacciSequence(count); // Call the method to print Fibonacci sequence
+        printFibonacciSequence(count);
     }
 
 }
