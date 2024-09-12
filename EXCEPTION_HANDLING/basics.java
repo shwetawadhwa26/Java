eee6ea2b7d5f3846f7d40b25adf2853e1daaf885
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-12 09:42:54 +0530

    exceptiobs

--- EXCEPTION_HANDLING/basics.java
+++ EXCEPTION_HANDLING/basics.java
@@ -5,10 +5,18 @@ public class basics {
         System.out.println("hello guys");
 
         try {
+            int result =9/0;
+
             System.out.println(a[8]);
             
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Tried to access the out of bound element ");
+        }
+        catch(ArithmeticException e){
+            System.out.println(e.getStackTrace());
+            System.out.println(e.getMessage());
+            System.out.println(e);
+
         }
         System.out.println("bye guys");
     }
