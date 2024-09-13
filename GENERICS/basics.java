58c164c7f0a4a9530fa3351cb467b6acb4f8ffbf
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-13 18:06:27 +0530

    generic class

--- GENERICS/basics.java
+++ GENERICS/basics.java
@@ -1,14 +1,26 @@
 public class basics {
     public static void main(String[] args) {
-        Dog d1=new Dog("123");
-        Dog d2 = new Dog("a23");
+        Dog<String,String>  d1=new Dog<>("123","TOM");
+        Dog<String,String>  d2= new Dog<>("a23","Leo");
+
+        Dog<Integer,String> d3= new Dog<>(122,"bonny");
+        System.out.println(d3.getId());
+        System.out.println(d2.getId());
+
+       // ArrayList<E> a = new ArrayList<>();
     }
 
 }
 
-class Dog{
-    String id;
-    public Dog(String id){
+class Dog<E,V>{
+    E id; //parameter
+    V name;
+    public Dog(E id,V name){
         this.id=id;
+        this.name=name;
+    }
+    E getId(){
+        return id;
+
     }
 }
