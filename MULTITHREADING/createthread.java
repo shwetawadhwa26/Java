1e26a838449f5411ebbd018e5b94aaef0411fdc2
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-16 09:42:04 +0530

    multithread

--- MULTITHREADING/createthread.java
+++ MULTITHREADING/createthread.java
@@ -2,9 +2,11 @@
 class MyThread extends Thread{
     @Override
     public void run(){
-        while (true) { 
+        int i =0;
+        while (i<10) { 
             System.out.println("My thread is running");
-        System.out.println("I am Happy");
+            System.out.println("I am Happy");
+            i++;
         }
         
     }
@@ -13,9 +15,12 @@ class MyThread extends Thread{
 class MyThread2 extends Thread{
     @Override
     public void run(){
-        while (true) { 
+        int i =0;
+        while (i<10) { 
             System.out.println("My thread is running");
-        System.out.println("I am Sad");
+        
+            System.out.println("I am Sad");
+        i++;
         }
         
     }
@@ -24,7 +29,8 @@ public class createthread {
     public static void main(String[] args){
         MyThread t1 = new MyThread();
         MyThread2 t2 = new MyThread2();
-
+        t1.start();
+        t2.start();
 
     }
     
