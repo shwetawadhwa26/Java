dd3f870b3e8632e82f8e6f972cd0e05c32ab5e98
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-20 15:36:54 +0530

    pratcise

--- ARRAYS/QUESTIONS/practise.java
+++ ARRAYS/QUESTIONS/practise.java
@@ -1,14 +1,33 @@
+// import java.util.Scanner;
+// public class practise {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         int arr[] = new int[10];
+//         for(int i =0;i<arr.length;i++){
+//             arr[i]=scanner.nextInt();
+//         }
+//         for(int i=arr.length-1;i>=0;i--){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+    
+// }
+
+
 import java.util.Scanner;
-public class practise {
+public class practise{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int arr[] = new int[10];
+        int sum =0;
+        int size = scanner.nextInt();
+        int arr[] = new int[size];
         for(int i =0;i<arr.length;i++){
             arr[i]=scanner.nextInt();
         }
-        for(int i=arr.length-1;i>=0;i--){
-            System.out.print(arr[i]+" ");
+        for(int i=0;i<arr.length;i++){
+            sum =sum+arr[i];
         }
+        System.out.println(sum);
     }
-    
 }
+
