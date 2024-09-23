ab7f254837ba04bbaa31df2a503a818592c9d63e
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-23 20:04:21 +0530

    strings

--- STRINGS/QUESTIONS/reversestring.java
+++ STRINGS/QUESTIONS/reversestring.java
@@ -14,16 +14,48 @@
 //     }
 // }
 
+// import java.util.Scanner;
+// public class reversestring{
+//     public static void main(String[] args){
+//         Scanner scanner = new Scanner(System.in);
+//         String input=scanner.nextLine();
+//         String words[] = input.split("\\ ");
+//         for (int i = words.length - 1; i >= 0; i--) {
+//             System.out.print(words[i]);
+//             if(i!=0){
+//                 System.out.print(" ");
+//             }
+//         }
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class reversestring {
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
+// String input = scanner.nextLine();
+// String[] words = input.split("\\ ");
+// for (int i = words.length - 1; i >= 0; i--) {
+// System.out.print(words[i]);
+// if(i!=0){
+// System.out.print(" ");
+// }
+// }
+// }
+// }
 
 import java.util.Scanner;
-public class reversestring{
-    public static void main(String[] args){
+
+public class reversestring {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input=scanner.nextLine();
+        String input = scanner.nextLine();
         String words[] = input.split("\\ ");
         for (int i = words.length - 1; i >= 0; i--) {
             System.out.print(words[i]);
-            if(i!=0){
+            if (i != 0) {
                 System.out.print(" ");
             }
         }
