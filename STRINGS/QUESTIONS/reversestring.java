87f6203476f5c063d94417a208edfa697aa8b8f5
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-09-28 20:31:07 +0530

    strings

--- STRINGS/QUESTIONS/reversestring.java
+++ STRINGS/QUESTIONS/reversestring.java
@@ -62,3 +62,19 @@ public class reversestring {
 
     }
 }
+import java.util.Scanner;
+
+public class reversestring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String words[] = input.split("\\ ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i]);
+            if (i != 0) {
+                System.out.print(" ");
+            }
+        }
+
+    }
+}
