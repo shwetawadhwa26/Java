5b1412ee75d021cd0014c943fb1dac7be4c9bfa6
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-10-07 21:51:08 +0530

    trees

--- TREES/basics.java
+++ TREES/basics.java
@@ -24,12 +24,31 @@ public class basics {
             return newNode;
         }
     }
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    
+
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
     public static void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
-        
+        // preorder(root);
+        inorder(root);
     }
     
 }
