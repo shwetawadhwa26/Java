2a08e3cffcc4f3991e7202e8db5ad3bf74b06fc4
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-10-16 09:51:37 +0530

    trees

---
+++ TREES/LEETCODE/AverageLevel.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>(); 
+        
+        if (root == null) { 
+            return result;
+        }
+        
+        Queue<TreeNode> q = new LinkedList<>();  
+        q.add(root);  
+        
+        while (!q.isEmpty()) {
+            int levelSize = q.size();  
+            double sum = 0; 
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = q.poll();  
+                sum += currentNode.val;
+                if (currentNode.left != null) {
+                    q.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    q.add(currentNode.right);
+                }
+            }
+            double averageLevel = sum / levelSize;
+            result.add(averageLevel);  
+        }
+        
+        return result; 
+    }  
+}
